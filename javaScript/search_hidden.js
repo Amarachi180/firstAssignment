f3746740ec95b25ad49fdcebfbe4ecd33304dde6
Amarachi180 <amaraedwina18@gmail.com>
2022-12-19 00:32:53 -0800

    assignment one

---
+++ javaScript/search_hidden.js
@@ -0,0 +1,25 @@
+function openSearch() {
+    document.getElementById("search").style.display = "flex";
+    document.querySelector(".hide-link-1").style.display = "none";
+    document.querySelector(".hide-link-2").style.display = "none";
+    document.querySelector(".hide-link-3").style.display = "none";
+    document.querySelector(".hide-link-4").style.display = "none";
+    document.querySelector(".hide-link-5").style.display = "none";
+    document.querySelector(".hide-link-6").style.display = "none";
+    document.querySelector(".hide-link-7").style.display = "none";
+    document.querySelector(".hide-link-8").style.display = "none";
+    document.querySelector(".hide-link-9").style.display = "none";
+}
+
+function closeSearch() {
+    document.getElementById("search").style.display = "none";
+    document.querySelector(".hide-link-1").style.display = "block";
+    document.querySelector(".hide-link-2").style.display = "block";
+    document.querySelector(".hide-link-3").style.display = "block";
+    document.querySelector(".hide-link-4").style.display = "block";
+    document.querySelector(".hide-link-5").style.display = "block";
+    document.querySelector(".hide-link-6").style.display = "block";
+    document.querySelector(".hide-link-7").style.display = "block";
+    document.querySelector(".hide-link-8").style.display = "block";
+    document.querySelector(".hide-link-9").style.display = "block";
+}
